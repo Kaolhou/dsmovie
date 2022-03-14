ba0fdb71af9dfc74b2712bba6c4d7d46caf77a04
Kaolhou <xdroid42@gmail.com>
2022-03-13 23:54:41 -0300

    err

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://andrem-dsmovie.herokuapp.comgit' ?? "http://localhost:8080"
+export const BASE_URL = 'https://andrem-dsmovie.herokuapp.com' ?? "http://localhost:8080"
